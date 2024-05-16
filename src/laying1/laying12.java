853ddcdd2a068809d3308500234eaf9503e1f08b
SAMIM <mdsamim852k2@gmail.com>
2024-05-16 14:37:18 +0600

    In sha allah

---
+++ src/laying1/laying12.java
@@ -0,0 +1,7 @@
+package laying1;
+
+public class laying12 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/laying2/helloworld.java
@@ -0,0 +1,7 @@
+package laying2;
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello Wrold");
+    }
+}
